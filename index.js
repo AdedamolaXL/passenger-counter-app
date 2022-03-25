1efd115094613aa467247a8b29adc934ecf472d0
adedamolaxl <adedamolaxl@outlook.com>
2022-03-26 00:57:50 +0100

    index.css

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+//document.getElementById assigns the html id attribute to variables saveEl, countEl
+//this DOM displays the results of the mathematical operations within the functions
+
+//the count variable initiialized as zero represents the passenger count entering the station
+
+//increment() executes the increase in passenger count
+//save() executes the storage of each counter value
+//the counter variable equated to the count variable receives the exact passenger count which is useful if you want to know how many passengers entered the station per day for example
+  //countEl.textContent = 0 resets the passenger count value to zero, this allows a fresh passenger count 
 
 let saveEl = document.getElementById('save-el')
 let countEl = document.getElementById('count-el')
@@ -12,6 +21,7 @@ function increment() {
 function save() {
   let counter = count + ' - '
   saveEl.textContent += counter
+
   countEl.textContent = 0
   count = 0
 }
