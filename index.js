1386aab23bcea5c24716258b3389eb3db17feb5a
adedamolaxl <adedamolaxl@outlook.com>
2022-05-10 17:45:12 +0100

    updated the code

--- index.js
+++ index.js
@@ -1,27 +1,19 @@
-//document.getElementById assigns the html id attribute to variables saveEl, countEl
-//this DOM displays the results of the mathematical operations within the functions
+let countEl = document.getElementById('count-el');  //countEl gets the 'count-el' element
+let saveEl = document.getElementById('save-el');  //saveEl gets the 'save-el' element
+let count = 0  //count is a variable storing the passenger count
 
-//the count variable initiialized as zero represents the passenger count entering the station
-
-//increment() executes the increase in passenger count
-//save() executes the storage of each counter value
-//the counter variable equated to the count variable receives the exact passenger count which is useful if you want to know how many passengers entered the station per day for example
-  //countEl.textContent = 0 resets the passenger count value to zero, this allows a fresh passenger count 
-
-let saveEl = document.getElementById('save-el')
-let countEl = document.getElementById('count-el')
-let count = 0
-
+//increment() executes the increase in passenger count whenever the Increment button is clicked
 function increment() {
-  count += 1
-  countEl.textContent = count
+  count += 1 //counts the increase in passenger by a value of 1
+  countEl.textContent = count  //displays the passenger count as text within the browser
 }
 
-
+//save() executes the storage of each counting sequence and then restarts all over again whenever the Save button is clicked
 function save() {
-  let counter = count + ' - '
-  saveEl.textContent += counter
+  let counter = count + ' - ' //stores the value of the count variable as incremented
+  saveEl.textContent += counter //saves the counter value and displays it as Previous entries within the browser
 
-  countEl.textContent = 0
-  count = 0
+  countEl.textContent = 0 // resets the passenger count as shown in the browser to zero
+  count = 0 //resets the count variable within js also to zero
 }
+
